ce39f8261f152a99a81a35315f39a747d449530c
Goffert van Gool <ruphin@ruphin.net>
2017-10-14 03:21:47 +0000

    v0.1.0

--- slidem-deck.js
+++ slidem-deck.js
@@ -1,31 +1,32 @@
-import{GluonElement,html}from'../gluonjs/gluon.js';import{onRouteChange,currentPath,currentHash}from'../gluon-router/gluon-router.js';import'../fontfaceobserver/fontfaceobserver.standalone.js';import'../gluon-keybinding/gluon-keybinding.js';const fontNode=document.createElement('link');fontNode.href='https://fonts.googleapis.com/css?family=Open+Sans+Condensed:700',fontNode.rel='stylesheet',document.head.appendChild(fontNode);const styleText=document.createTextNode(`
+import{GluonElement,html}from'../gluonjs/gluon.js';import{onRouteChange,currentPath,currentHash}from'../gluon-router/gluon-router.js';import'./slidem-slide.js';import'../fontfaceobserver/fontfaceobserver.standalone.js';import'../gluon-keybinding/gluon-keybinding.js';const styleText=document.createTextNode(`
   /* SLIDEM SHARED STYLE */
-  slidem-deck slidem-slide {
-    font-family: 'Open Sans Condensed', sans-serif;
-    font-size: 56px;
-    line-height: 1;
+  body {
+    margin: 0;
   }
+
   slidem-deck slidem-slide h1,
   slidem-deck slidem-slide h2,
   slidem-deck slidem-slide h3,
   slidem-deck slidem-slide h4,
   slidem-deck slidem-slide h5,
   slidem-deck slidem-slide h6,
-  slidem-deck slidem-slide p{
+  slidem-deck slidem-slide p {
     margin-top: 0px;
     margin-bottom: 0px;
   }
+
+  /* Avoid overflow which breaks width calculation */
+  slidem-deck slidem-slide [fit] {
+    font-size: 30px;
+  }
+
   slidem-deck slidem-slide a {
     color: inherit;
     text-decoration: none;
   }
-  slidem-deck slidem-slide slidem-reveal {
-    display: block;
-    opacity: 0;
+
+  [reveal] {
     transition: opacity 0.2s;
-  }
-  slidem-deck slidem-slide slidem-reveal[revealed] {
-    opacity: 1;
   }
 `),styleNode=document.createElement('style');styleNode.appendChild(styleText),document.head.appendChild(styleNode);class SlidemDeck extends GluonElement{get template(){return html`
       <div class="slides">
@@ -47,6 +48,9 @@ import{GluonElement,html}from'../gluonjs/gluon.js';import{onRouteChange,currentP
           left: 0;
           bottom: 0;
           right: 0;
+          font-family: 'sans-serif';
+          font-size: 56px;
+          line-height: 1;
         }
         .slides {
           height: 100%;
@@ -62,6 +66,7 @@ import{GluonElement,html}from'../gluonjs/gluon.js';import{onRouteChange,currentP
           display: flex;
           flex-flow: row;
           justify-content: center;
+          z-index: 10;
         }
         #progress div {
           height: 8px;
@@ -76,21 +81,6 @@ import{GluonElement,html}from'../gluonjs/gluon.js';import{onRouteChange,currentP
         #progress div.active {
           background: white;
         }
-        #forward {
-          right: 0;
-        }
-        #backward {
-          left: 0;
-        }
-        #forward, #backward {
-          position: absolute;
-          top: 0;
-          bottom: 0;
-          width: 20%;
-        }
-        #forward:hover, #backward:hover {
-          background: rgba(255,255,255,0.1);
-        }
       </style>
-    `}connectedCallback(){super.connectedCallback(),this.slides=Array.from(this.getElementsByTagName('slidem-slide')),this.slides.forEach((a)=>{a._steps=Array.from(a.getElementsByTagName('slidem-reveal')),this.$.progress.appendChild(document.createElement('div'))}),onRouteChange(()=>{this.previousSlide!==void 0&&(this.slides[this.previousSlide].active=!1,this.$.progress.children[this.previousSlide].classList.remove('active')),this.slides[this.currentSlide].active=!0,this.slides[this.currentSlide].step=this.currentStep+1,this.$.progress.children[this.currentSlide].classList.add('active'),this.previousSlide=this.currentSlide}),this.$.forward.onclick=()=>{this.slides[this.currentSlide].step<=this.slides[this.currentSlide].reveals.length?(window.history.pushState({},'',`${currentPath()}#slide-${this.currentSlide+1}/step-${this.slides[this.currentSlide].step+1}`),window.dispatchEvent(new Event('location-changed'))):this.currentSlide<this.slides.length-1&&(window.history.pushState({},'',`${currentPath()}#slide-${this.currentSlide+2}/step-1`),window.dispatchEvent(new Event('location-changed')))},this.$.backward.onclick=()=>{1<this.slides[this.currentSlide].step?(window.history.pushState({},'',`${currentPath()}#slide-${this.currentSlide+1}/step-${this.slides[this.currentSlide].step-1}`),window.dispatchEvent(new Event('location-changed'))):0<this.currentSlide&&(window.history.pushState({},'',`${currentPath()}#slide-${this.currentSlide}/step-${this.slides[this.currentSlide-1].reveals.length+1}`),window.dispatchEvent(new Event('location-changed')))},this.removeAttribute('loading');const a=()=>window.dispatchEvent(new Event('location-changed'));new FontFaceObserver('Open Sans Condensed',{weight:700}).load().then(a,a)}get currentSlide(){return(currentHash().match(/(?:slide-(\d+))?(?:\/step-(\d+|Infinity))?/)[1]||1)-1}get currentStep(){return(currentHash().match(/(?:slide-(\d+))?(?:\/step-(\d+|Infinity))?/)[2]||1)-1}}customElements.define(SlidemDeck.is,SlidemDeck);
+    `}connectedCallback(){super.connectedCallback(),this.slides=Array.from(this.getElementsByTagName('slidem-slide')),this.slides.forEach(()=>{this.$.progress.appendChild(document.createElement('div'))}),onRouteChange(()=>{this.slides[this.currentSlide].step=this.currentStep+1,this.slides[this.currentSlide].active=!0,this.slides[this.currentSlide].classList.add('animate-in');this.previousSlide===this.currentSlide||(this.previousSlide!==void 0&&(this.previousSlide<this.currentSlide?(this.slides[this.previousSlide].classList.add('animate-forward'),this.slides[this.currentSlide].classList.add('animate-forward'),this.slides[this.previousSlide].classList.remove('animate-backward'),this.slides[this.currentSlide].classList.remove('animate-backward')):(this.slides[this.previousSlide].classList.add('animate-backward'),this.slides[this.currentSlide].classList.add('animate-backward'),this.slides[this.previousSlide].classList.remove('animate-forward'),this.slides[this.currentSlide].classList.remove('animate-forward'))),this.fadeOutSlide!==void 0&&(this.slides[this.fadeOutSlide].classList.remove('animate-out'),this.fadeOutSlide!==this.currentSlide&&(this.slides[this.fadeOutSlide].active=!1)),this.previousSlide!==void 0&&(this.slides[this.previousSlide].classList.remove('animate-in'),this.slides[this.previousSlide].classList.add('animate-out'),this.$.progress.children[this.previousSlide].classList.remove('active'),this.fadeOutSlide=this.previousSlide),this.$.progress.children[this.currentSlide].classList.add('active'),this.previousSlide=this.currentSlide)}),this.$.forward.onclick=()=>{this.slides[this.currentSlide].step<=this.slides[this.currentSlide].reveals.length?(window.history.pushState({},'',`${currentPath()}#slide-${this.currentSlide+1}/step-${this.slides[this.currentSlide].step+1}`),window.dispatchEvent(new Event('location-changed'))):this.currentSlide<this.slides.length-1&&(window.history.pushState({},'',`${currentPath()}#slide-${this.currentSlide+2}/step-1`),window.dispatchEvent(new Event('location-changed')))},this.$.backward.onclick=()=>{1<this.slides[this.currentSlide].step?(window.history.pushState({},'',`${currentPath()}#slide-${this.currentSlide+1}/step-${this.slides[this.currentSlide].step-1}`),window.dispatchEvent(new Event('location-changed'))):0<this.currentSlide&&(window.history.pushState({},'',`${currentPath()}#slide-${this.currentSlide}/step-${this.slides[this.currentSlide-1].reveals.length+1}`),window.dispatchEvent(new Event('location-changed')))},this.removeAttribute('loading');const a=()=>{window.dispatchEvent(new Event('location-changed'))},b=this.getAttribute('font');b?(this.style.fontFamily=b,new FontFaceObserver(b).load().then(a,a)):a()}get currentSlide(){return(currentHash().match(/(?:slide-(\d+))?(?:\/step-(\d+|Infinity))?/)[1]||1)-1}get currentStep(){return(currentHash().match(/(?:slide-(\d+))?(?:\/step-(\d+|Infinity))?/)[2]||1)-1}}customElements.define(SlidemDeck.is,SlidemDeck);
 //# sourceMappingURL=slidem-deck.js.map
--- slidem-slide.js
+++ slidem-slide.js
@@ -1,24 +1,123 @@
 import{GluonElement,html}from'../gluonjs/gluon.js';class SlidemSlide extends GluonElement{get template(){return html`
       <div id="content"><slot id="slot"></slot></div>
       <style>
-        :host(:not([active])) {
-          display: none;
-        }
         :host {
-          display: flex;
+          position: absolute;
+          top: 0;
+          left: 0;
+          bottom: 0;
+          right: 0;
+          display: none;
           overflow: hidden;
-          height: 100%;
-          width: 100%;
           justify-content: center;
           align-items: center;
           background-size: cover;
         }
+        :host([active]) {
+          display: flex;
+        }
+        :host([active].animate-in) {
+          z-index: 2;
+        }
+        :host([active].animate-out) {
+          z-index: 0;
+        }
+        :host([slide-in]), :host([slide-out]), :host([zoom]) #content {
+          animation-duration: 0.4s;
+          animation-fill-mode: both;
+          animation-timing-function: ease-in-out;
+        }
+
+        @keyframes slide-in-in {
+          from {
+            transform: translateX(100vw);
+          }
+          to {
+            transform: translateX(0);
+          }
+        }
+
+        @keyframes slide-in-out {
+          from {
+            transform: translateX(0);
+          }
+          to {
+            transform: translateX(100vw);
+          }
+        }
+
+        :host([slide-in].animate-forward.animate-in) {
+          animation-name: slide-in-in;
+        }
+        :host([slide-in].animate-backward.animate-out) {
+          animation-name: slide-in-out;
+          z-index: 3;
+        }
+
+
+        @keyframes slide-out-in {
+          from {
+            transform: translateX(0);
+          }
+          to {
+            transform: translateX(-100vw);
+          }
+        }
+
+        @keyframes slide-out-out {
+          from {
+            transform: translateX(-100vw);
+          }
+          to {
+            transform: translateX(0);
+          }
+        }
+
+        :host([slide-out].animate-forward.animate-out) {
+          animation-name: slide-out-in;
+          z-index: 3;
+        }
+        :host([slide-out].animate-backward.animate-in) {
+          animation-name: slide-out-out;
+        }
+
+        @keyframes zoom-in {
+          from {
+            opacity: 0;
+            transform: scale(0);
+          }
+          to {
+            opacity: 1;
+            transform: scale(1);
+          }
+        }
+
+        @keyframes zoom-out {
+          from {
+            opacity: 1;
+            transform: scale(1);
+          }
+          to {
+            opacity: 0;
+            transform: scale(0);
+          }
+        }
+
+        :host([zoom].animate-in) #content {
+          animation-name: zoom-in;
+        }
+        :host([zoom].animate-out) #content {
+          animation-name: zoom-out;
+        }
+
         #content {
           width: 100%;
           max-width: 1080px;
-          height: 100%;
           max-height: 760px;
         }
+        :host(:not([center])) #content {
+          height: 100%;
+        }
       </style>
-    `}connectedCallback(){super.connectedCallback();const a=this.getAttribute('background');if(a)if(a.match(/^--[a-zA-Z-]*$/))this.style.background=`var(${a})`;else if(a.match(/^http/)||a.match(/^\//)){const b=this.getAttribute('darken-background');let c=a;b&&(c=`linear-gradient(rgba(0,0,0,${b}), rgba(0,0,0,${b})), url(${c})`),this.style.backgroundImage=c}else this.style.background=a;this.textNodes=Array.from(this.querySelectorAll('h1, h2, h3, h4, h5, h6, p')),this.textNodes.forEach((a)=>{null!==a.getAttribute('bold')&&(a.style.fontWeight='bold'),null!==a.getAttribute('italic')&&(a.style.fontStyle='italic'),null!==a.getAttribute('uppercase')&&(a.style.textTransform='uppercase'),null!==a.getAttribute('center')&&(a.style.textAlign='center'),null!==a.getAttribute('line-height')&&(a.style.lineHeight=a.getAttribute('line-height'));const b=a.getAttribute('color');null!==b&&(b.match(/^--[a-zA-Z-]*$/)?a.style.color=`var(${b})`:a.style.color=b)}),this.reveals=Array.from(this.getElementsByTagName('slidem-reveal'))}static get observedAttributes(){return['step','active']}attributeChangedCallback(a,b,c){if('step'===a){const a=+c;if(a>this.reveals.length+1)return void this.setAttribute('step',this.reveals.length+1);this.__setStep(a)}'active'===a&&null!==c&&this.__rescale()}set step(a){this.setAttribute('step',a)}get step(){return+this.getAttribute('step')||1}set active(a){a?this.setAttribute('active',''):this.removeAttribute('active')}get active(){return null!==this.getAttribute('active')}__setStep(a){this.reveals.forEach((b,c)=>{c<a-1?b.setAttribute('revealed',''):b.removeAttribute('revealed')})}__rescale(){this.textNodes.forEach((a)=>{if(null!==a.getAttribute('fit')){a.style.display='table';const b=parseFloat(window.getComputedStyle(a,null).getPropertyValue('font-size')),c=this.$.content.clientWidth;a.style.fontSize=`${Math.floor(b*c/a.clientWidth)-1}px`}})}}customElements.define(SlidemSlide.is,SlidemSlide);
+    `}connectedCallback(){super.connectedCallback();const a=this.getAttribute('background');if(a)if(a.match(/^--[a-zA-Z-]*$/))this.style.background=`var(${a})`;else if(a.match(/^http/)||a.match(/^\//)){const b=this.getAttribute('darken-background');let c=a;b&&(c=`linear-gradient(rgba(0,0,0,${b}), rgba(0,0,0,${b})), url(${c})`),this.style.backgroundImage=c}else this.style.background=a;this.textNodes=Array.from(this.querySelectorAll('h1, h2, h3, h4, h5, h6, p')),this.textNodes.forEach((a)=>{null!==a.getAttribute('bold')&&(a.style.fontWeight='bold'),null!==a.getAttribute('italic')&&(a.style.fontStyle='italic'),null!==a.getAttribute('uppercase')&&(a.style.textTransform='uppercase'),null!==a.getAttribute('center')&&(a.style.textAlign='center'),null!==a.getAttribute('line-height')&&(a.style.lineHeight=a.getAttribute('line-height'));const b=a.getAttribute('color');null!==b&&(b.match(/^--[a-zA-Z-]*$/)?a.style.color=`var(${b})`:a.style.color=b)}),this.layoutNodes=Array.from(this.getElementsByTagName('div')),this.layoutNodes.forEach((a)=>{null!==a.getAttribute('center')&&(a.style.display='flex',a.style.justifyContent='center',a.style.alignItems='center')}),this.reveals=Array.from(this.querySelectorAll('[reveal]'))}static get observedAttributes(){return['step','active']}attributeChangedCallback(a,b,c){if('step'===a){const a=+c;if(a>this.reveals.length+1)return void this.setAttribute('step',this.reveals.length+1);this.__setStep(a)}'active'===a&&null!==c&&this.__rescale()}set step(a){this.setAttribute('step',a)}get step(){return+this.getAttribute('step')||1}set active(a){a?this.setAttribute('active',''):this.removeAttribute('active')}get active(){return null!==this.getAttribute('active')}__setStep(a){this.reveals.forEach((b,c)=>{b.style.opacity=c<a-1?1:0})}__rescale(){this.textNodes.forEach((a)=>{if(null!==a.getAttribute('fit')){a.style.display='table';const b=parseFloat(window.getComputedStyle(a,null).getPropertyValue('font-size')),c=this.$.content.clientWidth;a.style.fontSize=`${Math.floor(b*c/a.clientWidth)-1}px`}})}}customElements.define(SlidemSlide.is,SlidemSlide);
 //# sourceMappingURL=slidem-slide.js.map
